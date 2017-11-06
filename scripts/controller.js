c38974d1167029021956cf533da0b4bd5f60f6e0
Daniel Zapata <danielzapata1019@gmail.com>
2017-11-06 15:36:53 -0500

    Modules good

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,1 +1,18 @@
-angular.module("myFirstApp",[])
+var app = angular.module("myFirstApp", []);
+app.controller('firstController', function ($scope) {
+    $scope.nombre = "Daniel";
+    $scope.comentarios = [
+        {
+            comentario: "Buen tutorial",
+            user: "Daniel"
+        },
+        {
+            comentario: "super",
+            user: "Juan"
+        }
+    ];
+    $scope.agregarComentario=function () {
+        $scope.comentarios.push($scope.nuevoComentario);
+        $scope.nuevoComentario={};
+    }
+});
